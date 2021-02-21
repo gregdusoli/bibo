c74a6bd81e973937219a5c3779f01bf43dbe0229
Guilherme Gomes <guilhermesg@hotmail.com>
2021-02-21 00:25:24 -0300

    Create Chapter Component

---
+++ src/components/Book/Chapter/index.tsx
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import bibleService from '../../../services/bible'
+import { Container } from './styles'
+
+export interface IChapter {
+  book: {
+    abbrev: string,
+    name: string,
+    author: string,
+    group: string,
+    version: string
+  },
+  chapter: {
+    number: number,
+    verses: number
+  },
+  verses: [{
+    number: number,
+    text: string
+  }]
+}
+
+interface IParams {
+  version: string
+  abbrev: string
+  chapter: string
+}
+
+const Chapter: React.FC = () => {
+  const [chapterVerses, setChapterVerses] = useState<IChapter | null>(null)
+  const { version, abbrev, chapter } = useParams<IParams>()
+
+  useEffect(() => {
+    const num = chapter ? parseInt(chapter) : 1
+    bibleService.getChapter(version, abbrev, num).then(result => setChapterVerses(result))
+  }, [abbrev, chapter, version])
+
+  return (
+    <Container>
+      <ul>
+        {chapterVerses?.verses.map(verse => (
+          <li key={verse.number}>
+            <strong>{verse.number}</strong> {verse.text}
+          </li>
+        ))}
+      </ul>
+    </Container>
+  )
+}
+
+export default Chapter
---
+++ src/components/Book/Chapter/styles.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const Container = styled.section`
+  * {
+    line-height: 1.7;
+  }
+
+  a {
+    color: inherit;
+  }
+`
