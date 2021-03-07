54778ac954358b6e64afd17bdc0b5fa07f3ce6b8
Guilherme Gomes <guilhermesg@hotmail.com>
2021-03-07 03:53:57 -0300

    Fix modal theme support

--- src/components/Template/Modal/index.tsx
+++ src/components/Template/Modal/index.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
-import { Modal, Button } from 'react-bootstrap'
+import { Button, Modal } from 'react-bootstrap'
+import { withTheme } from 'styled-components'
+import { ModalStyle } from './styles'
 
 export interface ModalProps {
   button: {
@@ -12,16 +14,17 @@ export interface ModalProps {
     variant: string;
     text: string;
   };
+  theme?: any;
   save?: {
     variant: string;
     text: string;
-    handleSave: () => {};
+    handleSave: () => void;
   };
 }
 
 const ModalComponent: React.FC<ModalProps> = (props) => {
   const [show, setShow] = useState(false)
-  const { button, title, body, close, save } = props
+  const { button, title, body, close, save, theme } = props
 
   const handleClose = () => setShow(false)
   const handleShow = () => setShow(true)
@@ -33,23 +36,28 @@ const ModalComponent: React.FC<ModalProps> = (props) => {
       </Button>
 
       <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>{title}</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>{body}</Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" type="dismiss" onClick={handleClose}>
-            {close.text}
-          </Button>
-          {save && (
-            <Button variant="primary" type="submit" onClick={save.handleSave}>
-              {save.text}
+        <ModalStyle>
+          <Modal.Header closeButton style={{ backgroundColor: theme.colors.background }}>
+            <Modal.Title>{title}</Modal.Title>
+          </Modal.Header>
+
+          <Modal.Body style={{ backgroundColor: theme.colors.background }}>{body}</Modal.Body>
+
+          <Modal.Footer style={{ backgroundColor: theme.colors.background }}>
+            <Button variant="secondary" type="dismiss" onClick={handleClose}>
+              {close.text}
             </Button>
-          )}
-        </Modal.Footer>
+
+            {save && (
+              <Button variant="primary" type="submit" onClick={save.handleSave}>
+                {save.text}
+              </Button>
+            )}
+          </Modal.Footer>
+        </ModalStyle>
       </Modal>
     </>
   )
 }
 
-export default ModalComponent
+export default withTheme(ModalComponent)
