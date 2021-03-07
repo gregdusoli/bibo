c2fe9a6443af7a0c9b1aa545f357108a1bca8045
Guilherme Gomes <guilhermesg@hotmail.com>
2021-03-07 02:23:13 -0300

    Resolve merge conflicts

--- src/hooks/context/Language/index.tsx
+++ src/hooks/context/Language/index.tsx
@@ -12,10 +12,6 @@ interface LanguageContextProps {
   language: LanguageProps
   changeLanguage: () => void
 }
-<<<<<<< HEAD
-=======
-// changeLanguage: React.Dispatch<React.SetStateAction<LanguageProps>>
->>>>>>> 22545bc453eac887e9641991959ea28f7b3d5b59
 
 const INITIAL_VALUE = {
   language: {
@@ -30,14 +26,9 @@ export const LanguageContextProvider: React.FC = ({ children }) => {
   const [lang, setLang] = useSavedState<LanguageProps>('language', INITIAL_VALUE.language)
 
   function changeLanguage () {
-<<<<<<< HEAD
     const newLanguage = lang.name === 'en' ? { name: Languages.BR } : { name: Languages.EN }
     setLang(newLanguage)
     i18n.changeLanguage(newLanguage.name)
-=======
-    setLang(lang.name === 'en' ? { name: Languages.BR } : { name: Languages.EN })
-    i18n.changeLanguage(lang.name)
->>>>>>> 22545bc453eac887e9641991959ea28f7b3d5b59
   }
 
   return (
